3d887575b827bfa7af5bbb5abd574c8a3c9da08f
Raytroll <raymandelphin@gmail.com>
2025-04-04 12:16:52 +0200

    Fix box collision + added launch direction

--- Assets/launch_sac.cs
+++ Assets/launch_sac.cs
@@ -8,6 +8,7 @@ public class launch_sac : MonoBehaviour
     public float force = 10;
     public float mass = 10;
     public float timing = 0.5f; // Value between 0.0 and 1.0 
+    public Vector2 dir = new Vector2(1.0f, 0.25f);
     private bool Has_launched = false;
 
     // Start is called before the first frame update
@@ -21,7 +22,7 @@ public class launch_sac : MonoBehaviour
     void Update() {
         if (Input.GetKeyDown(KeyCode.Space) && Has_launched == false) {
             float adjustedForce = force * mass * timing; // Calculate force based on mass and timing
-            Body.AddForce(new Vector3(adjustedForce, adjustedForce, 0), ForceMode.Impulse);
+            Body.AddForce(new Vector3(adjustedForce * dir.x, adjustedForce * dir.y, 0), ForceMode.Impulse);
             Body.angularVelocity = new Vector3(0, 0, -adjustedForce);
             Has_launched = true;
         }
